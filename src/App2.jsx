5564698a424650cc948584d18e789c134781bcff
Emily <emilys@genstudents.org>
2025-02-13 15:15:23 -0300

    :sparkles: Projeto inicialmente configurado

---
+++ src/App2.jsx
@@ -0,0 +1,41 @@
+// atalho rafce
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App2
+
+// rfce
+
+// Arrow Function
+//const App3 = () => <div></div>
+
+// Nomeação de Componente
+// PascalCase
+
+// Nomeação de variavel, função...
+// camelCase
+
+// export default, posso importar com qlqr nome e sem chaves
+// export 'sem default', só posso importar entre chaves e com o nome exportado
+
+// Selfie closing Tag
+// <App></App> ou <App />
+
+// Nomeação de classes em CSS
+// Metodologia BEM
+// Blocks
+// Elements
+// Modifiers
+// bloco__elemento
+// bloco__elemento--modificador
+// item-list__header
+//  Nomes compostosos são separados por -
+
+//  Tag vazia em React se chama Fragment ou Fragmento <></>
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+
+const ItemList = () => {
+  return (
+    <div>
+    
+      <div className='item-list'>
+
+  <div className='item-list__header'>
+      <h2>Artistas Populares</h2>
+      <a href='/'>Mostrar tudo</a>
+  </div>
+
+  <div className='item-list__container'>
+
+      <div className='single-item'>
+
+          <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                  <img 
+                  className='single-item__image' 
+                  src='https://i.scdn.co/image/ab67616100005174668a5535093f4cc53b1fef45'
+                  alt='Imagem artista X' />
+              </div>
+          
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+          </div>
+
+              <div className='single-item__texts'>
+                  <div className='single-item__2lines'>
+                      <p className='single-item__title'>Oruam</p>
+                      <p className='single-item__type'>Artista</p>   
+                  </div>
+              </div>
+      </div>
+      
+      <div className='single-item'>
+
+          <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                  <img 
+                  className='single-item__image' 
+                  src='https://i.scdn.co/image/ab67616100005174668a5535093f4cc53b1fef45'
+                  alt='Imagem artista X' />
+              </div>
+          
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+          </div>
+
+              <div className='single-item__texts'>
+                  <div className='single-item__2lines'>
+                      <p className='single-item__title'>Oruam</p>
+                      <p className='single-item__type'>Artista</p>   
+                  </div>
+              </div>
+      </div>
+      
+      <div className='single-item'>
+
+          <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                  <img 
+                  className='single-item__image' 
+                  src='https://i.scdn.co/image/ab67616100005174668a5535093f4cc53b1fef45'
+                  alt='Imagem artista X' />
+              </div>
+          
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+          </div>
+
+              <div className='single-item__texts'>
+                  <div className='single-item__2lines'>
+                      <p className='single-item__title'>Oruam</p>
+                      <p className='single-item__type'>Artista</p>   
+                  </div>
+              </div>
+      </div>
+      
+      <div className='single-item'>
+
+          <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                  <img 
+                  className='single-item__image' 
+                  src='https://i.scdn.co/image/ab67616100005174668a5535093f4cc53b1fef45'
+                  alt='Imagem artista X' />
+              </div>
+          
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+          </div>
+
+              <div className='single-item__texts'>
+                  <div className='single-item__2lines'>
+                      <p className='single-item__title'>Oruam</p>
+                      <p className='single-item__type'>Artista</p>   
+                  </div>
+              </div>
+      </div>
+      
+      <div className='single-item'>
+
+          <div className='single-item__div-image-button'>
+              <div className='single-item__div-image'>
+                  <img 
+                  className='single-item__image' 
+                  src='https://i.scdn.co/image/ab67616100005174668a5535093f4cc53b1fef45'
+                  alt='Imagem artista X' />
+              </div>
+          
+              <FontAwesomeIcon className='single-item__icon' icon={faCirclePlay} />
+          </div>
+
+              <div className='single-item__texts'>
+                  <div className='single-item__2lines'>
+                      <p className='single-item__title'>Oruam</p>
+                      <p className='single-item__type'>Artista</p>   
+                  </div>
+              </div>
+      </div>
+      
+  </div>
+      </div>
+    </div>
+  )
+}
+
+export default ItemList
